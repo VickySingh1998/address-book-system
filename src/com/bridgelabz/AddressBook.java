146f583b2dbcd4487db3f468e2020fde46bbb243
vickysingh1998 <vickysingh8697@gmail.com>
2022-07-19 23:43:26 +0530

    added: new feature to display added contact

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -3,7 +3,33 @@ package com.bridgelabz;
 import java.util.*;
 
 public class AddressBook {
+    private static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Contact> contactDetail = new ArrayList<>();
+
+    private static ArrayList<Contact> addContact() {
+        Contact contact = new Contact();
+        System.out.println("Enter First Name:");
+        contact.setFirstName(scanner.nextLine());
+        System.out.println("Enter Last Name:");
+        contact.setLastName(scanner.nextLine());
+        System.out.println("Enter Address:");
+        contact.setAddress(scanner.nextLine());
+        System.out.println("Enter City :");
+        contact.setCity(scanner.nextLine());
+        System.out.println("Enter State :");
+        contact.setState(scanner.nextLine());
+        System.out.println("Enter Zip Code :");
+        contact.setZipCode(scanner.next());
+        System.out.println("Enter Phone Number :");
+        contact.setPhoneNumber(scanner.next());
+        System.out.println("Enter Email :");
+        contact.setEmail(scanner.next());
+
+        contactDetail.add(contact);
+        return contactDetail;
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome To Address Book Program");
+        addContact();
+        System.out.println(contactDetail);
     }
 }
